15540f6b4271f954f5a99dc4d97ba03a9a3ccbb7
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-04-11 22:31:31 -0600

    OPP section added

---
+++ 25.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+
+
+    <?php 
+      // > Create a class = Create a new Data Type (template)
+      // > It is like a blueprint of a book
+      class Book {
+        var $title;
+        var $author;
+        var $pages;  
+      }
+
+      $book1 = new Book;
+      $book1->title = "Quantum Computing Fundamentals";
+      $book1->author = "Ximena Toledo";
+      $book1->pages = 50;
+
+      $book2 = new Book;
+      $book1->title = "Quantum Computing: Shor Algorithm";
+      $book1->author = "Ximena Toledo";
+      $book1->pages = 6;
+
+    
+      // ? Object: Instance of a class
+      // ? Class: Is like a blueprint, is a specification for what a x object is.
+    ?>
+  </body>
+</html>
